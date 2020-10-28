cde222edd4ded2e08d29a7c122f623c6dce13b5e
dasta <vickydasta@gmail.com>
2020-10-28 23:48:34 +0700

    clean up

--- main.go
+++ main.go
@@ -5,33 +5,33 @@ yet another url shortener built with Go
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
-    "fmt"
+	"math/rand"
 	"net/http"
-    "math/rand"
-    "strings"
+	"strings"
 )
 
-func genRandom(n int)string{
-        s := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
-        "t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O",
-        "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
+func genRandom(n int) string {
+	s := []string{"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
+		"t", "u", "v", "w", "x", "y", "z", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O",
+		"P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"}
 
-		temp := make([]string, n, n)
+	temp := make([]string, n, n)
 
-		for i := 0; i < n; i++ {
-			temp[i] = s[rand.Intn(len(s))]
-		}
-        rword := strings.Join(temp[:], "")
-        return rword
+	for i := 0; i < n; i++ {
+		temp[i] = s[rand.Intn(len(s))]
+	}
+	rword := strings.Join(temp[:], "")
+	return rword
 }
 
 func main() {
 	//db := make(map[string]string)
 
-    h1 := func(w http.ResponseWriter, _ *http.Request) {
-        io.WriteString(w, fmt.Sprintf("%s\n", genRandom(200)))
+	h1 := func(w http.ResponseWriter, _ *http.Request) {
+		io.WriteString(w, fmt.Sprintf("%s\n", genRandom(200)))
 	}
 
 	http.HandleFunc("/", h1)
